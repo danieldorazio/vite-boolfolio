b2b6e97ed5675f7679322de148f49c8ffb77a5a6
danieldorazio <danieldorazio.97@gmail.com>
2024-02-01 19:13:45 +0100

    Created a new ProjectCard component, which corresponds to a card for viewing a project. Used this component to view all projects received via API.

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,23 +1,36 @@
 <script>
 import axios from 'axios';
+import ProjectCard from './ProjectCard.vue';
 
 export default {
+
     data() {
-        return{
+        return {
             baseUrl: 'http://127.0.0.1:8000',
+            projects: [],
         }
     },
     created() {
         axios.get(`${this.baseUrl}/api/projects`)
-        .then((resp) => {
-            console.log(resp);
-        })
-    }
+            .then((resp) => {
+                this.projects = resp.data.result.data
+                // console.log(resp.data.result.data);
+            })
+    },
+    components: { ProjectCard },
 }
 
 </script>
 <template>
-    <h1>ciao</h1>
+    <div class="container">
+        <div class="row row-cols-3">
+            <div class="col" v-for="project in projects">
+
+                <ProjectCard :project="project" />
+
+            </div>
+        </div>
+    </div>
 </template>
 
 <style scoped lang="scss"></style>
---
+++ src/components/ProjectCard.vue
@@ -0,0 +1,28 @@
+<script>
+export default {
+    props: {
+        project: Object,
+    },
+    computed: {
+        truncateText () {
+            if (this.project.description && this.project.description.length > 150) {
+                return this.project.description.substring(0, 150) + "...";
+            }
+            return this.project.description;
+        }
+    }
+}
+</script>
+
+<template>
+    <div class="card" style="width: 18rem;">
+        <div class="card-body">
+
+            <h5 class="card-title">{{ project.title }}</h5>
+            <h6 class="card-subtitle mb-2 text-body-secondary">{{ project.slug }}</h6>
+            <p class="card-text">{{ truncateText }}</p>
+        </div>
+    </div>
+</template>
+
+<style scoped lang="scss"></style>
