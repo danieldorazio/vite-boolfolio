d868073cb0027ce3e49ea941126fa2a25f6a9585
danieldorazio <danieldorazio.97@gmail.com>
2024-02-02 17:46:21 +0100

    added navBar in AppMain

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,33 @@
 <script>
-import AppMain from './components/AppMain.vue';
-
-
 export default {
   data() {
-    return {};  
+    return {};
   },
-  components: { AppMain }
 }
 
 
 </script>
 
 <template>
-  <AppMain />
+  <header>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <div class="container-fluid">
+        <a class="navbar-brand" href="#">Navbar</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav me-auto mb-2 mb-lg-0">          
+            <li class="nav-item">
+              <router-link :to="{ name: 'home' }" class="nav-link active" >Home</router-link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  </header>
+
 
   <router-view></router-view>
 </template>
--- test.md
+++ test.md
@@ -2,10 +2,14 @@
 
 Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ipsam architecto tenetur numquam explicabo officia assumenda adipisci nulla ex delectus quasi voluptatibus laudantium cum reprehenderit placeat, nostrum illum velit recusandae fugit?
 
-# titolo B
+# titolo B2
 
 Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ipsam architecto tenetur numquam explicabo officia assumenda adipisci nulla ex delectus quasi voluptatibus laudantium cum reprehenderit placeat, nostrum illum velit recusandae fugit?
 
 # titolo C
+
+Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ipsam architecto tenetur numquam explicabo officia assumenda adipisci nulla ex delectus quasi voluptatibus laudantium cum reprehenderit placeat, nostrum illum velit recusandae fugit?
+
+# titolo D
 
 Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ipsam architecto tenetur numquam explicabo officia assumenda adipisci nulla ex delectus quasi voluptatibus laudantium cum reprehenderit placeat, nostrum illum velit recusandae fugit?
